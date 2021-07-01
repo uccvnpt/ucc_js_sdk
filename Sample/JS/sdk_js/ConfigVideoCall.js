ce8fcb09c2635e1cf3a03512d6b50d9450084be6
Nguyen Quang Sang <nguyenquangsang.dng@gmail.com>
2021-07-01 09:28:44 +0700

    Add comment code

--- Sample/JS/sdk_js/ConfigVideoCall.js
+++ Sample/JS/sdk_js/ConfigVideoCall.js
@@ -1,12 +1,4 @@
-// const ConfigVideoCall = {
-//     token_id: 'be400bf3-9b91-311b-e053-5f4fc10a3519',
-//     token_id_app: 'be400bf3-9b91-311b-e053-5f4fc10a3519',
-//     token_key:
-//         'MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAMiy2tvBR5iGiR2smm+/2dCgpAdqk2RdYAggcxKJYsPLurqiQwSrAOaUVoPPU7WS+o8uP246sD9UE+8rlmmTrpsCAwEAAQ==',
-//     client_id: '2Q0z4vJXaVRBwPyTIy6IZ236eWSBsbDadOGcqrkm',
-//     client_secret: 'IdbLaOwHUBp23bddqLGC0y8930NdqNTROKZ0s5Y8',
-// };
-
+// dev key
 const ConfigVideoCall = {
     token_id: 'c542f45d-b95d-5ae5-e053-6c1b9f0afb81',
     token_id_app: '01ebcf7e-1a5c-1080-82a1-10a45c074591',
@@ -16,11 +8,3 @@ const ConfigVideoCall = {
     client_id: '4IcYtqApSxeGWrTaEyjUzIHm4S57Ob4BLp1KeLdz',
     client_secret: 'B9bQDZ8g9GYw2ji9bqNhsqUG9SeEc0NlNePPrM5q',
 };
-
-// const ConfigVideoCall = {
-//     token_id: 'c17a7b3a-3d5b-2b05-e053-604fc10abbdb',
-//     token_key:
-//         'MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAKYWb8Oc3UZ6R9pL6AZMIoiWh2yiA4lzrdR1ziiFH90xeHFYwHL1hSp9vhfaFVnxjxl8pQ25MQjZOsNI3HNgso0CAwEAAQ==',
-//     client_id: 'pvPmhuIUyZg6gxUVWYBZh84m2AEow8fgMMGFEXDQ',
-//     client_secret: 'ptAuQGVvjROuq4RqYuD1dMw6keHBnPsY790qSfux',
-// };
